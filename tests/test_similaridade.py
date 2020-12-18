ed19fb346ccf48c0a538804af8277ad1b695695c
Ricart <ricarteusuo11@gmail.com>
2020-12-18 00:04:01 -0300

    test: verifica similaridade entre usuários sem itens em comum

--- tests/test_similaridade.py
+++ tests/test_similaridade.py
@@ -20,3 +20,16 @@ def test_similaridade():
     usuario1.add_opniao("Tá dando onda", 5)
 
     assert get_similaridade(usuario1, usuario2) == 0.19074357
+
+
+def test_similaridade_sem_itens_em_comum():
+    usuario1 = Usuario("Ricart", "ricart@email.com", "senha123")
+    usuario2 = Usuario("Glaymar", "glaymar@email.com", "mudar123")
+
+    usuario1.add_opniao("Deu a louca na chapeuzinho vermelha", 3)
+    usuario1.add_opniao("Rogue One", 5)
+
+    usuario2.add_opniao("Cinderela baiana", 5)
+    usuario2.add_opniao("A revolução dos bichos", 2)
+
+    assert get_similaridade(usuario1, usuario2) == 0
